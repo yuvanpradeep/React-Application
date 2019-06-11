e5674cc0e3dfe447c46cde29d9d16c3def165beb
yuvanpradeep <yuvanpradeep594@gmail.com>
2019-06-11 16:42:14 -0700

    Basics of JSX

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-
-
 const lists = ['India', 'England', 'Australia'];
-
-const olElement = React.createElement('ol', null, lists.map((lis, index) => React.createElement('li', {key: index}, lis)));
-
+//const olElement = React.createElement('ol', null, lists.map((lis, index) => React.createElement('li', {key: index}, lis)));
+const olElement = <div><h1>List of Countries</h1><ol>{lists.map((list, index) => <li key={index}> {list}</li>)} </ol></div>
 ReactDOM.render(olElement, document.getElementById('root'));
 
